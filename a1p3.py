aed37d824ec2436c6263959cc5f1825c27bdc962
zouber04 <i.zouber04@gmail.com>
2022-07-07 23:34:22 -0400

    Second version a1p3.py

--- a1p3.py
+++ a1p3.py
@@ -1,15 +1,13 @@
 def calculate_final_grade(deliverables,weights):
-    pass
+    final_grade = 0
+    for x in range(len(deliverables)):
+        grade = float(input(f"Enter {deliverables[x]} grade: "))
+        final_grade += grade * weights[x]
+    return final_grade
 
-
-
-assign = input('Enter Assignment Grade:')
-midterm = input('Enter Midterm Grade:')
-tutor= input('Enter tutorial Grade:')
-exam = input('Enter Exam Grade:')
-pres = input("Enter presentation mark: ")
-
-
-mark = assign*0.5+midterm*0.15+tutor*0.1+exam*0.25+pres*0.45
-
-print("Mark is: "+str(mark))
+if __name__ == "__main__":
+    mark = calculate_final_grade(
+        ["assignment","midterm", "tutorial", "exam"],
+        [0.5,0.15,0.1,0.25]
+    )
+    print(f"Final Grade: {mark:.2f}")
